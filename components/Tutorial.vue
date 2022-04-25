5ee6595234aca40d1a8785e013277b4593813a09
Ali Haji Aghakhani <aliaghakhani911@gmail.com>
2022-04-25 17:54:06 +0430

    initial

---
+++ components/Tutorial.vue
@@ -0,0 +1,14 @@
+<!-- Please remove this file from your project -->
+<template>
+ <BaseButton />
+</template>
+
+<script>
+import BaseButton from './elements-ui/BaseButton.vue'
+export default {
+  name: 'NuxtTutorial',
+  components:{
+    BaseButton,
+  }
+}
+</script>
---
+++ components/elements-ui/BaseButton.vue
@@ -0,0 +1,16 @@
+<template>
+<button class="button is-primary">click</button>
+</template>
+
+<script>
+export default {
+  name : 'BaseButton'
+}
+</script>
+
+<style lang="scss">
+
+   @import "bulma/sass/utilities/_all.sass";
+   @import "bulma/sass/elements/button.sass";
+
+</style>
